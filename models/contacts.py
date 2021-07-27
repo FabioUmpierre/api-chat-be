6c93fbf9d96fd4d26d393e8ac04497254e6037fa
Fábio Umpierre <fabio.umpierre@condoconta.com.br>
2021-07-27 00:06:19 -0300

    feat: added users and contacts resources

---
+++ models/contacts.py
@@ -0,0 +1,64 @@
+from . import db
+from models.user import UserModel
+
+
+class ContactsModel(db.Model):
+    __tablename__ = 'contacts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('user.id'))
+    contactUserId = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, userId, contactUserId):
+        self.userId = userId
+        self.contactUserId = contactUserId
+
+    def json(self):
+        return {
+            k: str(v) for k, v in self.__dict__.items()
+            if k != '_sa_instance_state'
+        }
+
+    @classmethod
+    def select_all_user_contacts(cls, user_id):
+        result = result = db.session.query(
+            ContactsModel, UserModel
+        ).join(
+            UserModel, ContactsModel.contactUserId == UserModel.id
+        ).filter(
+            ContactsModel.userId == user_id
+        ).all()
+        response = [{
+            'id': x.ContactsModel.contactUserId,
+            'name': x.UserModel.name,
+            'imageUrl': x.UserModel.imageUrl,
+        } for x in result]
+        return response
+
+    @classmethod
+    def find_contact(cls, id):
+        return cls.query.filter_by(id=id).first() or None
+
+    def save_contact(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def delete_contact(cls, userId, contactUserId):
+        contact_row = cls.query.filter(
+            ContactsModel.userId == userId,
+            ContactsModel.contactUserId == contactUserId
+        ).first()
+        if contact_row:
+            db.session.delete(contact_row)
+            db.session.commit()
+            return True
+
+    def verify_chat_exists(field1, field2):
+        result = db.session.query(
+            ContactsModel
+        ).filter(
+            ContactsModel.userId == field1, ContactsModel.contactUserId == field2
+        ).all()
+        if len(result) > 0:
+            return {"message": "contact already exists"}
---
+++ resources/contacts.py
@@ -0,0 +1,34 @@
+from flask_restful import Resource, reqparse
+from models.contacts import ContactsModel
+
+
+class ContactSearch(Resource):
+    def get(self, user_id):
+        result = ContactsModel.select_all_user_contacts(user_id)
+        return result
+
+
+class ContactCreate(Resource):
+    def delete(self, userId, contactUserId):
+        deleted = ContactsModel.delete_contact(userId, contactUserId)
+        deleted_user_contact = ContactsModel.delete_contact(
+            contactUserId, userId)
+        if deleted or deleted_user_contact:
+            return {'message': 'contact relationship deleted'}
+        return {'message': 'contact not found.'}, 404
+
+    def post(self, userId, contactUserId):
+        result = ContactsModel.verify_chat_exists(userId, contactUserId)
+        if result:
+            return result, 400
+
+        contact = ContactsModel(userId, contactUserId)
+        contact.save_contact()
+
+        result = ContactsModel.verify_chat_exists(contactUserId, userId)
+        if result:
+            return result
+
+        contact = ContactsModel(contactUserId, userId)
+        contact.save_contact()
+        return {'message': 'contact added is succefully!'}
